5b21e438a1472a2118054b6b3875d48274d65d2b
Nocommas555 <nocommas555@gmail.com>
2021-03-05 16:21:45 +0200

    fix movement not registering after refocusing window

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from Chelone import *
 
 # setting up a basic scene to test
-a = init(1000,500)
+Chelone = init(1000,500)
 
 loader = SpriteLoader()
-a.add_sprite(Sprite(loader.load("Untitled.png"),10,10),40)
+Chelone.add_sprite(Sprite(loader.load("Untitled.png"),10,10),40)
 
 
 class Player(Sprite):
@@ -13,38 +13,40 @@ class Player(Sprite):
 		print("setup Player")
 
 	def update(sprite):
+		global Chelone
 
-		if 'w' in pressed_keys:
+		if 'w' in Chelone.pressed_keys:
 			sprite.move(0,-1)
-		if 'a' in pressed_keys:
+		if 'a' in Chelone.pressed_keys:
 			sprite.move(-1,0)
-		if 's' in pressed_keys:
+		if 's' in Chelone.pressed_keys:
 			sprite.move(0,1)
-		if 'd' in pressed_keys:
+		if 'd' in Chelone.pressed_keys:
 			sprite.move(1,0)
 
-		if "q" in pressed_keys:
+		if "q" in Chelone.pressed_keys:
 			sprite.shedule_anim(loader.load("stick_figure"))
 			flag = False
 
-		if "e" in pressed_keys:
+		if "e" in Chelone.pressed_keys:
 			sprite.change_image(loader.load("Untitled.png"))
 
-		if "g" in pressed_keys:
+		if "g" in Chelone.pressed_keys:
 			sprite.change_image(loader.load("stick_figure/2.png"))
 
-		if "v" in pressed_keys:
+		if "v" in Chelone.pressed_keys:
 			sprite.change_image(clear_img)
 
 for i in range(1000):
 	spr = Player(loader.load("test.gif"))
-	a.add_sprite(spr)
+	Chelone.add_sprite(spr)
 
 
 
 while 1:
 	startTime = time()
-	a.advance_frame()
+	Chelone.advance_frame()
 	endTime = time()
 	elapsedTime = endTime - startTime
-	print(1./elapsedTime)
+
+	#print(Chelone.pressed_keys)
