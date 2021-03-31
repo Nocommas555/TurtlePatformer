f9b3599ee004696d0c32fa592de99d2eb8e8e2f7
Vlad <cyberlord2003@gmail.com>
2021-03-31 22:49:11 +0300

    adding ability to play sound via vlc on pyhton

---
+++ sound.py
@@ -0,0 +1,16 @@
+#plays file via vlc
+def _playsoundLinux(sound):
+	import os
+	os.system('cvlc ' + sound)
+
+from platform import system
+system = system()
+print(system)
+
+if system == 'Linux':
+	playsound = _playsoundLinux
+
+del system
+
+#test
+playsound('/home/vlad/Desktop/sound/file_example_MP3_1MG.mp3')
