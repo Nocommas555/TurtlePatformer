afbccb4f5649072b060af0fcb6eae259dcb53e3b
Nocommas555 <nocommas555@gmail.com>
2021-04-12 14:14:09 +0300

    add bg audio

--- sound.py
+++ sound.py
@@ -1,23 +1,28 @@
 from time import sleep
 import subprocess
+
 sounds = {}
 next_id = 0
 
 
 def sound_finished(id):
-	return sounds[next_id].poll() != None
+	return sounds[str(id)].poll() != None
 
 def kill_all_sounds():
 	for sound in sounds.values():
 		sound.kill()
 
 def _playsound_linux(file_source):
-	sounds[next_id] = subprocess.Popen(["aplay", file_source])
+	global next_id
+
+	sounds[str(next_id)] = subprocess.Popen(["aplay", file_source])
 	next_id+=1
 	return next_id-1
 
 def _playsound_win(file_source):
-	sounds[next_id] = subprocess.Popen(["python", "sound_win_slave.py", file_source])
+	global next_id
+
+	sounds[str(next_id)] = subprocess.Popen(["python", "sound_win_slave.py", file_source])
 	next_id+=1
 	return next_id-1
 
