2b300493d95d3a9ecd433201cb0bc0c46c1a6cc6
Nocommas555 <nocommas555@gmail.com>
2021-04-12 14:17:58 +0300

    add bg audio

--- sound.py
+++ sound.py
@@ -19,6 +19,13 @@ def _playsound_linux(file_source):
 	next_id+=1
 	return next_id-1
 
+def _playsound_darwin(file_source):
+	global next_id
+
+	sounds[str(next_id)] = subprocess.Popen(["afplay", file_source])
+	next_id+=1
+	return next_id-1
+
 def _playsound_win(file_source):
 	global next_id
 
@@ -32,4 +39,6 @@ if system == 'Linux':
 	playsound = _playsound_linux
 elif system == 'Windows':
 	playsound = _playsound_win
+elif system = 'Darwin':
+	playsound = _playsound_darwin
 del system
